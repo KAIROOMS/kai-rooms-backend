db15eab7b48c3fdba0cefbe968e14707b751b1f1
Salsa8630 <ncabilla86@gmail.com>
2025-07-29 20:01:23 +0700

    update cors origin

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: "https://kai-rooms.vercel.app" || "http://localhost:3000",
   credentials: true,
 }));
 app.use(bodyParser.json());
